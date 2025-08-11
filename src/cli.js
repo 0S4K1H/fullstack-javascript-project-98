1494291359dbae95939d8102bd2f9ce46c07ad8b
Mateo Cardenas <lmcardenas.gonzalez@gmail.com>
2025-08-11 17:46:20 -0500

    Fix: return del nombre en greetUser()

--- src/cli.js
+++ src/cli.js
@@ -4,4 +4,5 @@ export default function greetUser() {
   console.log('¡Bienvenido a Brain Games!');
   const name = readlineSync.question('¿Cuál es tu nombre? ');
   console.log(`¡Hola, ${name}!`);
+  return name; // ← 🔥 este return es clave
 }
