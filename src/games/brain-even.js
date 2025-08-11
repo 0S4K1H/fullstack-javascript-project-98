3f66393388b433174eac7f6802d38877586294aa
Mateo Cardenas <lmcardenas.gonzalez@gmail.com>
2025-08-11 18:26:18 -0500

    Fix: eliminar espacio final en línea para cumplir con lint

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,7 +4,7 @@ import greetUser from '../cli.js';
 const isEven = (num) => num % 2 === 0;
 
 export default function runEvenGame() {
-  const name = greetUser(); 
+  const name = greetUser();
 
   console.log('Responde "yes" si el número es par, de lo contrario responde "no".');
 
