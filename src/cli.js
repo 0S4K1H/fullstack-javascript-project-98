df26d20acfd88ea2f784894404f307e483bf86ea
Mateo <lcardenas.gonzalez@gmail.com>
2025-08-11 19:08:37 -0500

    Fix signos de exclamacion

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 export default function greetUser() {
-  console.log('Bienvenido a Brain Games!');
-  const name = readlineSync.question('Cual es tu nombre? ');
-  console.log(`Hola, ${name}!`);
+  console.log('¡Bienvenido a Brain Games!');
+  const name = readlineSync.question('¿Cuál es tu nombre? ');
+  console.log(`¡Hola, ${name}!`);
   return name;
 }
